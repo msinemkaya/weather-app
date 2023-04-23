f15bcfb88e9187ef883ae772789e523509eb032d
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-04-23 19:38:30 +0300

    feat: added operations to App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import LoadingSpinner from './components/LoadigSpinner';
 import Navbar from './components/Navbar';
 import SubInfo from './components/SubInfo';
@@ -6,10 +6,19 @@ import WeatherCard from './components/WeatherCard';
 import WeatherSlider from './components/WeatherSlider';
 import { TiWeatherWindy } from "react-icons/ti";
 import { WiThermometerExterior} from "react-icons/wi"
+import useApi from './hooks/useApi';
+import * as Weather from './api/weather';
 
 function App() {
 
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [search, setSearch] = useState('')
+  const weatherApi = useApi(Weather.getCurrentWeather)
+  const [weatherData, setWeatherData] = useState({})
+
+  useEffect(() => {
+    weatherApi.request({params: {q: search}}).then(() => setIsLoading(false))
+  }, [search])
 
   // let content = (
   //     <WeatherCard name={'Aydın, TR'} degree={'15'} date={'April 22, 1999'} />
@@ -25,23 +34,20 @@ function App() {
   // }
 
   return (
-    <div className='container'>
-      <Navbar />
-      <WeatherCard name={'Aydın, TR'} degree={'15'} date={'April 22, 1999'} />
-      <div className='sub-info-container'>
-        <SubInfo info={"humidity"} icon={<WiThermometerExterior/>} value={'50%'}/>
-        <SubInfo info={"wind"} icon={<TiWeatherWindy />} value={'19km/h'}/>
+    <>
+      <div className='container'>
+        <Navbar setSearch={setSearch} value={search}/>
+        {!isLoading && <>
+          <WeatherCard name={weatherApi.data.location.name} degree={weatherApi.data.current.temp_c} date={'April 22, 1999'} />
+          <div className='sub-info-container'>
+            <SubInfo info={"humidity"} icon={<WiThermometerExterior/>} value={`${weatherApi.data.current.humidity}%`}/>
+            <SubInfo info={"wind"} icon={<TiWeatherWindy />} value={`${weatherApi.data.current.wind_kph}km/H`}/>
+          </div>
+          <WeatherSlider />
+        </>}
       </div>
-      <WeatherSlider />
-      {/* <LoadingSpinner /> */}
-    </div>
-
-    // <>
-    //   <div className='container'>
-    //     <Navbar />
-    //     <LoadingSpinner /> 
-    //   </div>  
-    // </> 
+      {isLoading && <LoadingSpinner /> }
+    </>
   );
 }
 
