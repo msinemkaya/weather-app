10e58e62a0b0c1298e611b5d51c383cada71703e
Halil Hakan Karabay <hakankarabay@icloud.com>
2023-04-23 19:38:11 +0300

    feat: passed parameters to Navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,24 +2,24 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlassLocation } from '@fortawesome/free-solid-svg-icons';
 import { useState } from 'react';
 
-export default function Navbar() {
+export default function Navbar({setSearch, value}) {
 
-  const [value, setValue] = useState('')
-
-  const handleSubmit = () => {
-    
-  }
+  // const [value, setValue] = useState('')
+  //
+  // const handleSubmit = () => {
+  //
+  // }
 
   const handleChange = ({target}) => {
-    setValue(target.value)
-    console.log(value)
+    setSearch(target.value)
+    // console.log(value)
   }
 
   return (
     <>
       <nav className='navbar'>
         <h1 className='logo'>Weather.</h1>
-        <form action='' className='search' onSubmit={handleSubmit}>
+        <form action='' className='search'>
           <input type='text' className='search-input' value={value} onChange={handleChange}/>
           <button className=''>
             <FontAwesomeIcon
